e595666a2a7724397a92686ddcd8e0c14388d708
ccadran <clariocadran33@gmail.com>
2024-02-04 11:52:39 +0100

    add colors variables

--- src/app/experience/page.js
+++ src/app/experience/page.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Dialogues from "../components/Dialogues/dialogues";
 import dataIntro from "../../data/intro.json";
+import Layout from "../components/Layout/layout";
 
 export default function () {
   return (
-    <div>
+    <Layout>
       <Dialogues data={dataIntro.intro} />
-    </div>
+    </Layout>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <Link href="/experience">intro</Link>
     </main>
   );
