b4e8f5826e09d8f83aa36d1a1f363e87784f743c
ccadran <clariocadran33@gmail.com>
2024-02-06 12:47:34 +0100

    start colorPicker

--- src/app/experience/madeleine/Games/colorPicker/page.js
+++ src/app/experience/madeleine/Games/colorPicker/page.js
@@ -1,11 +1,24 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import LayoutNav from "../../../../components/LayoutNav/layoutNav";
+import Data from "../../../../../data/painting.json";
+import styles from "./colorPicker.module.scss";
 
 export default function page() {
+  const data = Data.madeleine;
+  const [currentColor, setCurrentColor] = useState();
+  console.log(data);
   return (
-    <>
-      <div>colorpicker</div>
+    <LayoutNav>
+      <div className={styles.consignes}>
+        <p>Retrouve la couleur du drap en cliquant sur la bonne couleur</p>
+      </div>
+      <div className={styles.paintingContainer}>
+        <img src="" alt="" />
+      </div>
       <Link href="/experience/madeleine/transiSlider">navigate to transi</Link>
-    </>
+    </LayoutNav>
   );
 }
