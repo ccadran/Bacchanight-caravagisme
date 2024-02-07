00cb485a59f36609b96c636a06c9e93bb33092cb
aznorf <61015776+Aznorf@users.noreply.github.com>
2024-02-07 14:55:09 +0100

    feat: functionalities

--- src/app/experience/sebastien/Games/switch/page.js
+++ src/app/experience/sebastien/Games/switch/page.js
@@ -1,10 +1,32 @@
-import Link from "next/link";
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { register } from 'swiper/element/bundle';
+register();
 
 export default function page() {
   return (
     <>
-      <div>puzzle</div>;
+      <swiper-container>
+        <swiper-slide>
+          <Image src={'/images/sebastien/LIVRE.png'} width={650} height={500} />
+        </swiper-slide>
+        <swiper-slide>
+          <Image src={'/images/sebastien/FIOLE.png'} width={650} height={500} />
+        </swiper-slide>
+        <swiper-slide>
+          <Image
+            src={'/images/sebastien/FLECHE.png'}
+            width={650}
+            height={500}
+          />
+        </swiper-slide>
+        <swiper-slide>
+          <Image src={'/images/sebastien/POMME.png'} width={650} height={500} />
+        </swiper-slide>
+      </swiper-container>
+
       <Link href="/experience/madeleine/machine">navigate to machine</Link>
     </>
   );
