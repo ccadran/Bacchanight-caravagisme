f6df62402f06d096b65fbeeeb38fe91b67d32f8b
aznorf <61015776+Aznorf@users.noreply.github.com>
2024-02-07 15:33:42 +0100

    feat: slider

--- src/app/experience/sebastien/Games/switch/page.js
+++ src/app/experience/sebastien/Games/switch/page.js
@@ -1,31 +1,53 @@
+'use client';
 import React from 'react';
+import Slider from 'react-slick';
+
 import Link from 'next/link';
 import Image from 'next/image';
 
-import { register } from 'swiper/element/bundle';
-register();
-
 export default function page() {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+  };
   return (
     <>
-      <swiper-container>
-        <swiper-slide>
-          <Image src={'/images/sebastien/LIVRE.png'} width={650} height={500} />
-        </swiper-slide>
-        <swiper-slide>
-          <Image src={'/images/sebastien/FIOLE.png'} width={650} height={500} />
-        </swiper-slide>
-        <swiper-slide>
+      <Slider {...settings}>
+        <div>
+          <Image
+            src={'/images/sebastien/LIVRE.png'}
+            width={650}
+            height={500}
+            alt={'Slide 1'}
+          />
+        </div>
+        <div>
+          <Image
+            src={'/images/sebastien/FIOLE.png'}
+            width={650}
+            height={500}
+            alt={'Slide 2'}
+          />
+        </div>
+        <div>
           <Image
             src={'/images/sebastien/FLECHE.png'}
             width={650}
             height={500}
+            alt={'Slide 3'}
           />
-        </swiper-slide>
-        <swiper-slide>
-          <Image src={'/images/sebastien/POMME.png'} width={650} height={500} />
-        </swiper-slide>
-      </swiper-container>
+        </div>
+        <div>
+          <Image
+            src={'/images/sebastien/POMME.png'}
+            width={650}
+            height={500}
+            alt={'Slide 1'}
+          />
+        </div>
+      </Slider>
 
       <Link href="/experience/madeleine/machine">navigate to machine</Link>
     </>
