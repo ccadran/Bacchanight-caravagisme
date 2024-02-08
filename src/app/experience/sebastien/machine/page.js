ec437d77750ef33a1cfef08d06eba5fbf45a44d0
ccadran <clariocadran33@gmail.com>
2024-02-08 10:48:01 +0100

    add machine component

--- src/app/experience/sebastien/machine/page.js
+++ src/app/experience/sebastien/machine/page.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import React from "react";
 import Layout from "../../../components/Layout/layout";
+import Machine from "@/app/components/Machine/machine";
 
 export default function page() {
   return (
     <Layout>
-      <div>machine</div>
-      <Link href="/experience/outro/">navigate to outro</Link>
+      <Machine link={"/experience/outro/"} />
     </Layout>
   );
 }
