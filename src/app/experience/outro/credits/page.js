7cd58198783457df6452fd972e5f9d6f3f6079a5
ccadran <clariocadran33@gmail.com>
2024-02-08 12:59:44 +0100

    end screen

---
+++ src/app/experience/outro/credits/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>crédits</div>;
+}
--- src/app/experience/outro/end/page.js
+++ src/app/experience/outro/end/page.js
@@ -2,22 +2,36 @@
 import Link from "next/link";
 import React from "react";
 import { useSearchParams } from "next/navigation";
+import LayoutNav from "@/app/components/LayoutNav/layoutNav";
+import styles from "./end.module.scss";
 
 export default function page() {
   const searchParams = useSearchParams();
   const search = searchParams.get("wichEnd");
   return (
-    <>
-      <h1>LA FIN</h1>
-      {search === "1" ? (
-        <Link href={`/experience/outro/otherEnd?index=3`}>
-          Rejouer depuis le début
-        </Link>
-      ) : (
-        <Link href={`/experience/outro/otherEnd?index=2`}>
-          Rejouer depuis le début 2
-        </Link>
-      )}
-    </>
+    <LayoutNav>
+      <div className={styles.end}>
+        <div className={styles.endPhrase}>
+          {search === "1"
+            ? "Bravo d'avoir aidé"
+            : "Vous avec laissé la censur gagné"}
+        </div>
+        <div className={styles.otherEnd}>
+          {search === "1" ? (
+            <Link href={`/experience/outro/otherEnd?index=3`}>
+              Découvrir l'autre fin
+            </Link>
+          ) : (
+            <Link href={`/experience/outro/otherEnd?index=2`}>
+              Découvrir l'autre fin{" "}
+            </Link>
+          )}
+        </div>
+        <div className={styles.controller}>
+          <Link href={`/`}>Rejouer</Link>
+          <Link href={`/experience/outro/credits`}>Crédits</Link>
+        </div>
+      </div>
+    </LayoutNav>
   );
 }
