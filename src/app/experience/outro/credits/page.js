5e582cd122bfca3bc60b7174445655ec988c2353
nin0dev <ninberber@gmail.com>
2024-02-09 11:58:18 +0100

    feat: design implemented

--- src/app/experience/outro/credits/page.js
+++ src/app/experience/outro/credits/page.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React from 'react';
+import styles from './credits.module.scss';
 
 export default function page() {
-  return <div>crédits</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Crédits</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.rectangle}>
+          <h2>Design</h2>
+          <p>KAHLAOUI Soufiyan</p>
+        </div>
+        <div className={styles.rectangle}>
+          <h2>Design</h2>
+          <p>MADELEINE Clément</p>
+        </div>
+        <div className={styles.rectangle}>
+          <h2>Écriture</h2>
+          <p>DEULCEUX Timothée</p>
+        </div>
+        <div className={styles.rectangle}>
+          <h2>Écriture</h2>
+          <p>ANDRIAMIHAJARIVO Yvan</p>
+        </div>
+        <div className={styles.rectangle}>
+          <h2>Développement</h2>
+          <p>BERBER Nino</p>
+        </div>
+        <div className={styles.rectangle}>
+          <h2>Développement</h2>
+          <p>CADRAN Clario</p>
+        </div>
+      </div>
+    </div>
+  );
 }
