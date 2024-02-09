2b4cdc6caf244468cc3b26980b686c353c01157a
ccadran <clariocadran33@gmail.com>
2024-02-09 10:49:23 +0100

    align right when police speak

--- src/app/components/Dialogue/dialogue.js
+++ src/app/components/Dialogue/dialogue.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./dialogue.module.scss";
 
 export default function dialogue({ dialogue, className, speaker }) {
+  console.log(speaker);
   return (
-    <div className={`${styles.dialogue} ${className}`}>
+    <div className={`${styles.dialogue} ${className} ${styles[speaker]}`}>
       <h4>{speaker}</h4>
       <p>{dialogue}</p>
     </div>
