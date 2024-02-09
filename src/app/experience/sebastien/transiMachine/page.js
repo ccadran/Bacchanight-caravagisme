5dd1e8b807f58a8bc385205a0d75db297389d7df
ccadran <clariocadran33@gmail.com>
2024-02-09 11:46:45 +0100

    deployment modif

--- src/app/experience/sebastien/transiMachine/page.js
+++ src/app/experience/sebastien/transiMachine/page.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Dialogues from "../../components/Dialogues/dialogues";
-import dataSebastien from "../../../data/sebastien.json";
+import Dialogues from "../../../components/Dialogues/dialogues";
+import dataSebastien from "../../../../data/sebastien.json";
 import Layout from "../../../components/Layout/layout";
 
 export default function () {
