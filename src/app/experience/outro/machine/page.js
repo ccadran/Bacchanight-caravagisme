62efc5e238fde004c04263578334410f1fcf9f9e
ccadran <clariocadran33@gmail.com>
2024-02-09 12:19:59 +0100

    fix deployment

--- src/app/experience/outro/machine/page.js
+++ src/app/experience/outro/machine/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import LayoutNav from "../../../components/LayoutNav/layoutNav";
 import Machine from "../../../components/Machine/machine";
 
-export default function Machine() {
+export default function MachineOutro() {
   return (
     <LayoutNav>
       <Machine link={"/experience/outro/afterGame"} />
